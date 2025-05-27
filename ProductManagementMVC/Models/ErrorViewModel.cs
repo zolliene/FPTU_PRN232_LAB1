f1fab5bdd41ff42051152f7632199a9877114238
zollie <thaonguyen07112004@gmail.com>
2025-05-27 11:27:55 +0700

    Add project files.

---
+++ ProductManagementMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProductManagementMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
